35cdab7ac6e1404afd5f4012c4bcb4effbddd319
Yaguchi-Haruo <takasumi.nakamura.2177@gmail.com>
2023-12-23 00:26:50 +0900

    remove: font設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,14 @@
 import "@/app/globals.css";
-import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import Sidebar from "./_components/Sidebar";
 import Link from "next/link";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <div className="min-h-screen flex flex-col">
           <header className="bg-blue-500 text-white p-4">
             <Link href="/">アプリタイトル</Link>
