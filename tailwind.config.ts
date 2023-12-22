51fb30b0d2890ab85f5e3e3bb06aaabd9dcce08e
Yaguchi-Haruo <takasumi.nakamura.2177@gmail.com>
2023-12-23 00:16:16 +0900

    settings: fontを追加する

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
